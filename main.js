d9b7079e88998b3b4fafabdcbeab8a2c1a4d5048
Travis CI <stephane.langlois@scopyleft.fr>
2017-12-20 14:07:20 +0100

    Finalisation et refactoring

---
+++ main.js
@@ -0,0 +1,26 @@
+const Hash = {
+  get: () => document.location.hash.substring(1),
+  onChange: () => Files.import()
+    .then(json => {
+      Articles.display(json.body)
+      Categories.display(Categories.get())
+    })
+}
+
+const Tpl = {
+  getElement: tplName =>
+    document
+      .querySelector(`template[name=${tplName}]`)
+      .cloneNode(true)
+      .content
+      .querySelector('[data-tpl=element]')
+}
+
+const Files = {
+  import: () => {
+    // const json = 'https://api.daktary.com/infolab-cd33/datalunch-/tree/master/fiches'
+    const json = './fiches.json'
+    return fetch(json)
+      .then(response => response.json())
+  }
+}
---
+++ sections.js
@@ -0,0 +1,61 @@
+const Categories = {
+  get: () =>
+    Array.from(
+      document.querySelectorAll('.categories li')
+    ).map(li => li.innerText),
+  getByFiles: files =>
+    files
+      .filter(file => file.meta) //if file a des meta
+      .map(file => file.meta.categorie) // renvoie la categorie
+      .filter((elt, pos, arr) => arr.indexOf(elt) === pos) // supprime les doublons en testant la position des éléments
+      .sort(),
+  display: categories => {
+    const container = document.querySelector('.categories')
+    container.innerHTML = ''
+    categories.forEach(category => {
+      const tpl = Tpl.getElement('categories')
+      const tplTitle = tpl.querySelector('[data-cat=titre]')
+      tplTitle.href = '#' + category
+      tplTitle.append(category)
+      if (category === Hash.get()) {
+        tplTitle.classList.add('selected')
+      }
+      container.append(tpl)
+    })
+  }
+}
+
+const Articles = {
+  getIcon: level => {
+    return {
+      débutant: 'fa-thermometer-empty',
+      intermédiaire: 'fa-thermometer-quarter',
+      avancé: 'fa-thermometer-three-quarters',
+      expert: 'fa-thermometer-full'
+    }[level]
+  },
+  display: files => {
+    const container = document.querySelector('.fiches')
+    const hash = Hash.get()
+    Articles.displayByCategory(container, files, hash)
+  },
+  displayByCategory: (container, files, categorie) => {
+    container.innerHTML = ''
+    files
+      .filter(file => file.meta)
+      .filter(file => file.meta.categorie === categorie)
+      .forEach(file => {
+        const tpl = Tpl.getElement('fiches')
+        const tplTitle = tpl.querySelector('[data-fiche=titre]')
+        const tplDesc = tpl.querySelector('[data-fiche=description]')
+        const tplLevel = tpl.querySelector('[data-fiche=niveau]')
+        const tplMore = tpl.querySelector('[data-fiche=plus]')
+        tplTitle.append(file.meta.title)
+        tplDesc.append(file.meta.description)
+        tplLevel.classList.add(Articles.getIcon(file.meta.niveau))
+        tplLevel.title = file.meta.niveau
+        tplMore.href = file.url
+        container.append(tpl)
+    })
+  }
+}
