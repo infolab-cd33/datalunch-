76a34ef05ca1b1cf050215d27bb3bd8e12a4361e
Travis CI <stephane.langlois@scopyleft.fr>
2017-12-21 12:36:24 +0100

    Affichage du contenu et de la licence d'une fiche

--- sections.js
+++ sections.js
@@ -17,7 +17,7 @@ const Categories = {
       const tplTitle = tpl.querySelector('[data-cat=titre]')
       tplTitle.href = '#categorie/' + category
       tplTitle.append(category)
-      if (category === Hash.get().replace('categorie/','')) {
+      if (category === Hash.get()) {
         tplTitle.classList.add('selected')
       }
       container.append(tpl)
@@ -36,8 +36,16 @@ const Articles = {
   },
   display: files => {
     const container = document.querySelector('.fiches')
-    const category = Hash.get().replace('categorie/','')
-    Articles.displayByCategory(container, files, category)
+    if (Hash.getType() === 'categorie') {
+      const category = Hash.get()
+      Articles.displayByCategory(container, files, category)
+    } else if (Hash.getType() === 'fiche') {
+      const file = files.filter(file => file.name === Hash.get())[0]
+      const tpl = document.querySelector('template[name=fiche]').cloneNode(true).content
+      tpl.querySelector('[data-fiche=licence]').append(file.meta.licence)
+      tpl.querySelector('[data-fiche=content]').innerHTML = file.body
+      container.append(tpl)
+    }
   },
   displayByCategory: (container, files, categorie) => {
     container.innerHTML = ''
